a288b6ab0ca7520d0f15c815b7a227464d8ebaed
rapidworks <rapidworks@naver.com>
2023-08-09 16:07:30 +0900

    Update AppDelegate.swift

--- GitHub Test/AppDelegate.swift
+++ GitHub Test/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+// 20230809
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
