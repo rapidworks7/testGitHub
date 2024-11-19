dfbda01b2ad59ec7db380120845c93e8fd7745b8
rapidworks <rapidworks@naver.com>
2024-11-19 11:59:32 +0900

    bb

--- GitHub Test/AppDelegate.swift
+++ GitHub Test/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print("start 1")
+        print("start 2")
         return true
     }
 
